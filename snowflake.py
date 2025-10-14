6c1a8d87a085ce2d9a45a20ae9c9fa5da950374e
jorgenll <jorgenll@stud.ntnu.no>
2025-10-14 16:19:34 +0200

    endre størrekse

--- snowflake.py
+++ snowflake.py
@@ -1,22 +1,23 @@
 from turtleplotlib import Turtle
 import matplotlib.pyplot as plt
-def snowflake(turtle, depth):
+
+def snowflake(turtle, depth, step):
     if depth == 0:
-        turtle.forward(8)          # utfør F
-    else:                          # F ->
-        snowflake(turtle, depth-1)  #      F
-        turtle.left(60)             #        +
-        snowflake(turtle, depth-1)  #          F
-        turtle.right(60)            #            -
-        turtle.right(60)            #              -  
-        snowflake(turtle, depth-1)  #                F
-        turtle.left(60)             #                  +
-        snowflake(turtle, depth-1)  #                    F
-def complete_flake(t):
+        turtle.forward(step)
+    else:
+        snowflake(turtle, depth - 1, step)
+        turtle.left(60)
+        snowflake(turtle, depth - 1, step)
+        turtle.right(120)
+        snowflake(turtle, depth - 1, step)
+        turtle.left(60)
+        snowflake(turtle, depth - 1, step)
+
+def complete_flake(t, depth=3, step=8):
     for _ in range(3):
-        snowflake(t, 3)
+        snowflake(t, depth, step)
         t.right(120)
     plt.show()
 
 t = Turtle(interactive=False)
-complete_flake(t)
+complete_flake(t, depth=3, step=3) 
